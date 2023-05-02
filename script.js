18284784fe0fff13bc8934b78661cc0569bcd431
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-05-02 07:51:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let btn=document.querySelector("input");
+btn.addEventListener("click",()=>{
+	let x=document.getElementById("colorSelect");
+	x.remove(x.selectedIndex);
+})
